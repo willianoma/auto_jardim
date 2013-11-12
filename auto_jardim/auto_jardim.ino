eee614393d24bff621997b7046015ea05e0bfdc0
willianoma <willian.movel@gmail.com>
2013-11-12 01:29:03 -0200

    Acoplado a Boia Manual

--- auto_jardim/auto_jardim.ino
+++ auto_jardim/auto_jardim.ino
@@ -4,66 +4,57 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Servo motor;
 
-int intervaloInicio = 10;//segundos
-int intervaloFim = 5;//segundos
+int tempoDeEspera = 15;//segundos
+int tempoDoCiclo = 25;//Segundos
 
 void setup() {
   lcd.begin(16, 2);
   motor.attach(10);
   Serial.begin(9600);
+  motor.write(0);
 }
 
 void loop() {
-  msgAlerta("Mudou de ciclo");
-  //inicioLCD();
-  tempoInicio(intervaloInicio*1000); 
-  motor.write(180);
-  msgAlerta("Mudou de ciclo");
-  fimLCD();
-  tempoFim(intervaloFim*1000); 
-  motor.write(90);
+  
+  cicloemEspera(tempoDeEspera*1000); 
+  iniciarCiclo(tempoDoCiclo*1000); 
+
 
   
+
 } //Fim do loop
-void inicioLCD(){
-//lcd.clear();  
-lcd.setCursor(0,0);
-lcd.print("Tempo p/ Inicio");
-}
-
-void fimLCD(){
-lcd.clear(); 
-lcd.setCursor(0,0);
-lcd.print("Tempo p/ Fim");
-}
 
 
-void tempoInicio(int tempo){
+void iniciarCiclo(int tempo){
+      lcd.clear();
+      Alerta("Iniciar ciclo");
+      lcd.clear();
+      motor.write(0);
       tempo=tempo/1000;
+      lcd.setCursor(0,0);
+      lcd.print("Tempo p/ Fim");
+      for (int i=0; i<=(tempo); i++){
+        lcd.setCursor(0,1);
+        lcd.print((tempo)-i); 
+        delay(1000); 
+      }  
+}
+
+void cicloemEspera(int tempo){ 
+      lcd.clear();
+      Alerta("Terminar ciclo");
+      lcd.clear();
+      motor.write(35);
+      tempo=tempo/1000;         
+      lcd.setCursor(0,0);
+      lcd.print("Tempo p/ Inicio");
       for (int i=0; i<=(tempo); i++){
       lcd.setCursor(0,1);
       lcd.print((tempo)-i); 
-      inicioLCD();
-      delay(1000);
-      lcd.clear();  
-  }
-}
-
-void tempoFim(int tempo){
-      for (int i=0; i<=(tempo/1000); i++){
-      lcd.setCursor(0,1);
-      lcd.print((tempo/1000)-i); 
       delay(1000);
     }
 }
 
-void ligarAgua(){
-  motor.write(90); 
-}
-
-void desligarAgua(){
-  motor.write(180);
-}
 
 
   void transicao(){  
@@ -79,30 +70,37 @@ void desligarAgua(){
    
   } //fim transicao
   
+   void Alerta(String msg){
+     
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(msg); 
+      delay(2000);
+   }
+  
+  
   
   void msgAlerta(String msg){
-    lcd.clear();
     int tamanho = msg.length();
     String texto = "";
     int spaco = (16-tamanho)/2;
     
     for (int i=0; i<spaco;i++){
-    texto += " ";
-        
-    efeitoDuasLinhas(texto+=msg);
+      texto += " ";
+      String valor =  texto+=msg;   
+    Serial.println(valor);
+    efeitoDuasLinhas(valor);
     delay(200);
-    lcd.clear();
     }
    } //fim msgAlenta
  
   void efeitoDuasLinhas(String valor){
   for (int i=0; i<=2; i++){
         for (int j=0; j<=1;j++){
-        lcd.setCursor(0,j);
-        lcd.print(valor); 
+          lcd.setCursor(0,j);
+          lcd.print(valor); 
         delay(300);
         lcd.clear(); 
         }
      }
   } //fim efeitoDuasLinhas
-  
